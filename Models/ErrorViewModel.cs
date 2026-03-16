bb4b54d1160cf15b4651d624cb102a555d31158f
DaniGuti21 <142607254+DaniGuti21@users.noreply.github.com>
2026-03-16 00:24:21 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIGRE_PYME.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
